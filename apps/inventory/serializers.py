3319a55abea957a1af079a94c7773eb2e2ccd8e0
Yogesh-MG <yogesh.m.gowdru@gmail.com>
2026-05-03 10:40:40 +0530

    product variant

--- apps/inventory/serializers.py
+++ apps/inventory/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apps.accounts.models import FarmerProfile, User
+from apps.accounts.models import FarmerProfile, User, ProductVariant
 from .models import Category, SubCategory, Product, InventoryBatch, ProductBenefit
 
 class FarmerSerializer(serializers.ModelSerializer):
